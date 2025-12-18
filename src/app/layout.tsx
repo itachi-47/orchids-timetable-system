be5b1d1285c52a159b696ce5c5c45feb9d890272
itachi-47 <varunpahuja2005@gmail.com>
2025-12-18 12:31:06 +0530

    Auto-sync after agent response

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" style={{ backgroundColor: "white" }}>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
